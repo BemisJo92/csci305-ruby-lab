b1f67396144ccf68abf6fb27c22c4118202433e7
BemisJo92 <bidinc.33@gmail.com>
2018-02-12 17:00:58 -0700

    Committing to work on another computer

--- ruby_lab.rb
+++ ruby_lab.rb
@@ -1,4 +1,3 @@
-
 #!/usr/bin/ruby
 ###############################################################
 #
@@ -10,36 +9,135 @@
 ###############################################################
 
 $bigrams = Hash.new # The Bigram data structure
-$name = "<firstname> <lastname>"
+$name = "John Bemis"
+$title
+$index = 1
+$titleList = Array.new
+$createTitle = ""
+$createCounter = 0
 
-def clean_up_title(song_title)
+def cleanup_title(song_title)
 	extract(song_title)
-	elim_text(song_title)
+	$title.downcase!
+	#print song_title
+	#puts $title
+	return $title
 end
 
+#Retrieves only the characters after the third <SEP>, aka the song
 def extract(song_title)
-	pattern = /<SEP>[\w\s]*$/
+	pattern = /<SEP>/
 	if pattern =~ song_title
-		new_title = "#{$&}"
+		$title = "#{$'}"
 	end
-	pattern2 = /<SEP>/
-	if pattern2 =~ new_title
-		final_title = "#{$'}"
+	#pattern2 = /<SEP>/
+	if pattern =~ $title
+		$title = "#{$'}"
+	end
+	#pattern3 = /<SEP>/
+	if pattern =~ $title
+		$title = "#{$'}"
+	end
+	#puts third_title
+	#pattern4 = /[\w*\s*]/
+	#if pattern4 =~ third_title
+		#final_title = "#{$&}"
+	#end
+	elim_text()
+end
+
+#Removes the garbage after the actual song that we want
+def elim_text
+	#pattern = /\(|\[|\{|\\|\/|\_|\-|\:|\"|\`|\+|\=|\*|feat./
+	pattern = /[(\[\{\\\/_\-:"`+=*]|feat./
+	#pattern = /\(/
+	if pattern =~ $title
+		$title = "#{$`}"
+	else
+		$title = $title
+	end
+	punct()
+	#$title = final_title
+end
+
+#Removes all the punctuation in each song and replaces it with whitespace
+def punct
+	#pattern = /\?|¿|!|¡|\.|;|&|@|%|#\|/
+	pattern = /[?¿!¡.;&@%#|]/
+	if pattern =~ $title
+		$title.gsub!(pattern, "")
 	end
 end
 
-def elim_text(song_title)
-	pattern = /<SEP>[\w|\s]*(\(|\[|\{|\\|\/|\_|\-|\:|\"|\`|\+|\=|\*|\ feat.)/
+#Finds only the english character songs
+def english_chars(song_title)
+	pattern = /\w*\s*/
 	if pattern =~ song_title
-		second_title = "#{$`}"
+		second_title = "#{$&}"
+	else
+		final_title = song_title
 	end
-	second_pattern = /<SEP>/
-	if second_pattern =~ second_title
-		third_title = "#{$'}"
+	$title = second_title
+end
+
+#Creates the bigram for a specfic word, and the words that follow that one specific word
+def buildBigram(word)
+	$titleList.each do |title|
+		pattern = /#{word}/
+		if pattern =~ title
+			secondWord = "#{$'}"
+			#$bigrams = {"#{word}": $index}
+		end
+		pattern2 = /\A \w* /
+		if pattern2 =~ secondWord
+			removeSpaces = "#{$&}"
+		end
+		pattern3 = /\S\w*\S/
+		if pattern3 =~ removeSpaces
+			if $bigrams[word] == nil
+				$bigrams[word] = Hash.new(0)
+			end
+			$bigrams[word]["#{$&}"] += 1
+		end
 	end
-	third_pattern = /(\(|\[|\{|\\|\/|\_|\-|\:|\"|\`|\+|\=|\*|\ feat.)/
-	if third_pattern =~ third_title
-		final_title = "#{$`}"
+end
+
+#Builds a bigram for every word in every title and the words following each word
+def testBigram(title)
+	$titleList = title.split
+	for i in 0..$titleList.length - 2
+		$bigrams[$titleList[i]]
+		if $bigrams[$titleList[i]] == nil
+			$bigrams[$titleList[i]] = Hash.new(0)
+		end
+		$bigrams[$titleList[i]][$titleList[i+1]] += 1
+	end
+end
+
+#Finds the most common word following a word that is passed in
+def mcw(word)
+	#buildBigram(word)
+	mostCommon = 0
+	key = ""
+	$bigrams[word].each do |secondWord, frequency|
+		if frequency > mostCommon
+			mostCommon = frequency
+			key = secondWord
+		end
+	end
+	return key
+end
+
+def create_title(startWord)
+	$createTitle << "#{startWord} "
+	$createCounter += 1
+	begin
+		nextWord = mcw(startWord)
+		if($createCounter >= 20)
+			return
+		else
+			create_title(nextWord)
+		end
 	end
 end
 
@@ -48,8 +146,29 @@ def process_file(file_name)
 	puts "Processing File.... "
 
 	begin
-		IO.foreach(file_name) do |line|
-			# do something for each line
+		if RUBY_PLATFORM.downcase.include? 'mswin'
+			file = File.open(file_name)
+			unless file.eof?
+				file.each_line do |line|
+					# do something for each line (if using windows)
+					title = cleanup_title(line)
+				end
+				#$bigrams.inspect
+			end
+			file.close
+		else
+			IO.foreach(file_name, encoding: "utf-8") do |line|
+				# do something for each line (if using macos or linux)
+				#print line
+				title = cleanup_title(line)
+				testBigram(title)
+				#$titleList.push(title)
+			end
+			#create_title("jim")
+			#puts $createTitle
+			#puts mcw("happy")
+			#puts $bigrams.inspect
+			#puts $bigrams[keys[0]]
 		end
 
 		puts "Finished. Bigram model built.\n"
@@ -72,6 +191,11 @@ def main_loop()
 	process_file(ARGV[0])
 
 	# Get user input
+	choice = a
+	puts "Enter a choice [Enter q for quit]:"
+	while(choice != q)
+		case choice
+		when
 end
 
 if __FILE__==$0
